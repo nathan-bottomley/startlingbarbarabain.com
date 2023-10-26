b82b02a73b9b56eb2700e632dac63aadad509d0d
Nathan Bottomley <nathan.bottomley@gmail.com>
2023-10-27 10:25:48 +1100

    Added episode pages and open graph data

--- src/_posts/_posts.11tydata.js
+++ src/_posts/_posts.11tydata.js
@@ -1,6 +1,7 @@
 module.exports = {
   tags: ['post'],
   permalink: '/{{ episodeNumber }}/',
+  layout: 'layouts/post.liquid',
   eleventyComputed: {
     episodeFile: data => `SBB ${data.episodeNumber}, ${data.title}.mp3`
   }
