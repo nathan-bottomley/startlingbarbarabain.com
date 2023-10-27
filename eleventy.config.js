57ecca6ae7f055d8ee77b2a4b93fb149c2e4562b
Nathan Bottomley <nathan.bottomley@gmail.com>
2023-10-27 12:13:07 +1100

    Added site icons

--- eleventy.config.js
+++ eleventy.config.js
@@ -43,6 +43,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy('src/css')
   eleventyConfig.addPassthroughCopy('src/fonts')
+  eleventyConfig.addPassthroughCopy({ 'src/icons': '/' })
 
   return {
     dir: {
