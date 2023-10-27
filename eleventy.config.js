e4f7708d5cb952a13ee52bb345f4f6fdc478563c
Nathan Bottomley <nathan.bottomley@gmail.com>
2023-10-27 17:42:25 +1100

    Fixed copying of icons

--- eleventy.config.js
+++ eleventy.config.js
@@ -52,7 +52,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy('src/css')
   eleventyConfig.addPassthroughCopy('src/fonts')
-  eleventyConfig.addPassthroughCopy({ 'src/icons': '/' })
+  eleventyConfig.addPassthroughCopy({ 'src/_icons': '/' })
 
   return {
     dir: {
