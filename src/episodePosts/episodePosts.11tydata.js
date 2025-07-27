7ffb488cf8e17261226c755b6b669da139beb0b9
Nathan Bottomley <nathan.bottomley@gmail.com>
2025-07-27 20:31:52 +1000

    Updated to Podcaster 2.0.0 alpha

--- src/episodePosts/episodePosts.11tydata.js
+++ src/episodePosts/episodePosts.11tydata.js
@@ -8,16 +8,6 @@ export default {
       if (data.imageFile) return data.imageFile
 
       return `${slugify(data.title, { lower: true, remove: /[’']/g })}.jpg`
-    },
-    'episode.episodeNumber' (data) {
-      if (data.episode?.episodeNumber) {
-        return data.episode?.episodeNumber
-      }
-
-      const matchResult = data.page.fileSlug.match(/^episode-(\d+)/)
-      if (matchResult) {
-        return parseInt(matchResult[1])
-      }
     }
   }
 }
