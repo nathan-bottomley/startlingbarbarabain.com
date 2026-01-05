6babeb4ffce2fce9790c18487a306da3776e1a05
Nathan Bottomley <nathan.bottomley@gmail.com>
2026-01-05 21:28:31 +1100

    Simplified eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,3 @@
 import neostandard from 'neostandard'
 
-export default [
-  ...neostandard(),
-  {
-    rules: {
-      '@stylistic/comma-dangle': ['error', 'never']
-    }
-  }
-]
+export default neostandard({})
