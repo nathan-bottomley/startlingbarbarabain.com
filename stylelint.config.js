8a4c03507dc52440f572dc136420e9c6076d83ec
Nathan Bottomley <nathan.bottomley@gmail.com>
2026-02-14 14:29:24 +1100

    Updates the package; added stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: [
+    'stylelint-config-recess-order',
+    'stylelint-config-standard'
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'no-duplicate-selectors': null
+  }
+}
