b31349d76efc784ce9bc38fd7f89b321cd986b16
Nathan Bottomley <nathan.bottomley@gmail.com>
2026-02-15 17:19:03 +1100

    Moved viewport meta tag earlier and conditionally loaded media-chrome

--- src/episode-posts/episode-posts.11tydata.js
+++ src/episode-posts/episode-posts.11tydata.js
@@ -3,6 +3,7 @@ import slugify from 'slugify'
 export default {
   tags: ['podcastEpisode'],
   layout: 'layouts/post.liquid',
+  includeAudioPlayer: true,
   eleventyComputed: {
     imageFile (data) {
       if (data.imageFile) return data.imageFile
